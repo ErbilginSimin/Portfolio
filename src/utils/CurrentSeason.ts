bea8a0d8cff7793a127a382c3074f2ae2184a803
ErbilginSimin <simin.erbilgin@oclock.school>
2024-09-12 17:56:26 +0200

    ✨feature:  Update getCurrentSeason function

--- src/utils/CurrentSeason.ts
+++ src/utils/CurrentSeason.ts
@@ -3,15 +3,33 @@ export const getCurrentSeason = ():
   | 'summer'
   | 'autumn'
   | 'winter' => {
-  const month = new Date().getMonth() + 1; // getMonth() is zero-based
+  const now = new Date(Date.now()); // Current date
+  const month = now.getMonth() + 1; // January is 0
+  const day = now.getDate(); // 1-31
+
   // console.log(month);
-  if (month >= 3 && month <= 5) {
-    return 'spring';
-  } else if (month >= 6 && month <= 8) {
-    return 'summer';
-  } else if (month >= 9 && month <= 11) {
-    return 'autumn';
-  } else {
-    return 'winter';
+  switch (month) {
+    case 3:
+      return day >= 21 ? 'spring' : 'winter';
+    case 4:
+    case 5:
+      return 'spring';
+    case 6:
+      return day >= 21 ? 'summer' : 'spring';
+    case 7:
+    case 8:
+      return 'summer';
+    case 9:
+      return day >= 23 ? 'autumn' : 'summer';
+    case 10:
+    case 11:
+      return 'autumn';
+    case 12:
+      return day >= 21 ? 'winter' : 'autumn';
+    case 1:
+    case 2:
+      return 'winter';
+    default:
+      return 'winter';
   }
 };
